0baac44ecb3106f09b8017d0865538e3b2f1b29d
Juhyeok0603 <111416825+Juhyeok0603@users.noreply.github.com>
2024-11-25 10:36:24 +0900

    Add files via upload

---
+++ test1/dbconn.php
@@ -0,0 +1,16 @@
+<?php
+
+
+#db 연결
+$servername = 'localhost';
+$username = 'root';
+$password = '';
+$db = 'userdata';
+
+$connect= mysqli_connect($servername,$username,$password,$db);
+
+if(!$connect)die("연결에 실패 하였습니다.".mysqli_connect_error());
+
+mysqli_set_charset($connect,"utf8");
+
+?>
